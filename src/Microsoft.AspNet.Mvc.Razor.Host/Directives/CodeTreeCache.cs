2bbfc6ecda917b04f006d65777b0fb56a24fa340
Pranav K <prkrishn@hotmail.com>
2015-02-06 18:37:08 -0800

    Results of parsing _ViewStart files should be cached
    Fixes #1016

---
+++ src/Microsoft.AspNet.Mvc.Razor.Host/Directives/CodeTreeCache.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Concurrent;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Razor.Generator.Compiler;
+using Microsoft.Framework.Expiration.Interfaces;
+
+namespace Microsoft.AspNet.Mvc.Razor.Directives
+{
+    /// <summary>
+    /// Default implementation of <see cref="ICodeTreeCache"/>.
+    /// </summary>
+    public class CodeTreeCache : ICodeTreeCache
+    {
+        private readonly IFileProvider _fileProvider;
+        private readonly ConcurrentDictionary<string, CodeTreeCacheEntry> _codeTreeCache;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="CodeTreeCache"/>.
+        /// </summary>
+        /// <param name="fileProvider">The application's <see cref="IFileProvider"/>.</param>
+        public CodeTreeCache(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+            _codeTreeCache = new ConcurrentDictionary<string, CodeTreeCacheEntry>(StringComparer.Ordinal);
+        }
+
+        /// <inheritdoc />
+        public CodeTree GetOrAdd([NotNull] string pagePath,
+                                 [NotNull] Func<IFileInfo, CodeTree> getCodeTree)
+        {
+            CodeTreeCacheEntry cacheEntry;
+            if (_codeTreeCache.TryGetValue(pagePath, out cacheEntry))
+            {
+                var expirationTrigger = cacheEntry.ExpirationTrigger;
+                if (!expirationTrigger.IsExpired)
+                {
+                    return cacheEntry.CodeTree;
+                }
+            }
+
+            var file = _fileProvider.GetFileInfo(pagePath);
+            // GetOrAdd is invoked for each _ViewStart that might potentially exist in the path.
+            // We can avoid performing file system lookups for files that do not exist by caching
+            // negative results and adding a Watch for that file.
+            var codeTree = file.Exists ? getCodeTree(file) : null;
+            var entry = new CodeTreeCacheEntry
+            {
+                ExpirationTrigger = _fileProvider.Watch(pagePath),
+                CodeTree = codeTree
+            };
+
+            _codeTreeCache[pagePath] = entry;
+            return codeTree;
+        }
+
+        private class CodeTreeCacheEntry
+        {
+            public CodeTree CodeTree { get; set; }
+
+            public IExpirationTrigger ExpirationTrigger { get; set; }
+        }
+    }
+}
---
+++ test/Microsoft.AspNet.Mvc.Razor.Host.Test/Directives/CodeTreeCacheTest.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNet.Mvc.Razor.Directives;
+using Microsoft.AspNet.Razor.Generator.Compiler;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Razor.Host.Directives
+{
+    public class CodeTreeCacheTest
+    {
+        [Fact]
+        public void GetOrAdd_ReturnsCachedEntriesOnSubsequentCalls()
+        {
+            // Arrange
+            var path = @"Views\_ViewStart.cshtml";
+            var mockFileProvider = new Mock<TestFileProvider> { CallBase = true };
+            var fileProvider = mockFileProvider.Object;
+            fileProvider.AddFile(path, "test content");
+            var codeTreeCache = new CodeTreeCache(fileProvider);
+            var expected = new CodeTree();
+
+            // Act
+            var result1 = codeTreeCache.GetOrAdd(path, fileInfo => expected);
+            var result2 = codeTreeCache.GetOrAdd(path, fileInfo => { throw new Exception("Shouldn't be called."); });
+
+            // Assert
+            Assert.Same(expected, result1);
+            Assert.Same(expected, result2);
+            mockFileProvider.Verify(f => f.GetFileInfo(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public void GetOrAdd_ReturnsNullValues_IfFileDoesNotExistInFileProvider()
+        {
+            // Arrange
+            var path = @"Views\_ViewStart.cshtml";
+            var mockFileProvider = new Mock<TestFileProvider> { CallBase = true };
+            var fileProvider = mockFileProvider.Object;
+            var codeTreeCache = new CodeTreeCache(fileProvider);
+            var expected = new CodeTree();
+
+            // Act
+            var result1 = codeTreeCache.GetOrAdd(path, fileInfo => expected);
+            var result2 = codeTreeCache.GetOrAdd(path, fileInfo => { throw new Exception("Shouldn't be called."); });
+
+            // Assert
+            Assert.Null(result1);
+            Assert.Null(result2);
+            mockFileProvider.Verify(f => f.GetFileInfo(It.IsAny<string>()), Times.Once());
+        }
+
+        [Fact]
+        public void GetOrAdd_UpdatesCache_IfFileExpirationTriggerExpires()
+        {
+            // Arrange
+            var path = @"Views\Home\_ViewStart.cshtml";
+            var fileProvider = new TestFileProvider();
+            fileProvider.AddFile(path, "test content");
+            var codeTreeCache = new CodeTreeCache(fileProvider);
+            var expected1 = new CodeTree();
+            var expected2 = new CodeTree();
+
+            // Act 1
+            var result1 = codeTreeCache.GetOrAdd(path, fileInfo => expected1);
+
+            // Assert 1
+            Assert.Same(expected1, result1);
+
+            // Act 2
+            fileProvider.GetTrigger(path).IsExpired = true;
+            var result2 = codeTreeCache.GetOrAdd(path, fileInfo => expected2);
+
+            // Assert 2
+            Assert.Same(expected2, result2);
+        }
+
+        [Fact]
+        public void GetOrAdd_UpdatesCacheWithNullValue_IfFileWasDeleted()
+        {
+            // Arrange
+            var path = @"Views\Home\_ViewStart.cshtml";
+            var fileProvider = new TestFileProvider();
+            fileProvider.AddFile(path, "test content");
+            var codeTreeCache = new CodeTreeCache(fileProvider);
+            var expected1 = new CodeTree();
+
+            // Act 1
+            var result1 = codeTreeCache.GetOrAdd(path, fileInfo => expected1);
+
+            // Assert 1
+            Assert.Same(expected1, result1);
+
+            // Act 2
+            fileProvider.DeleteFile(path);
+            fileProvider.GetTrigger(path).IsExpired = true;
+            var result2 = codeTreeCache.GetOrAdd(path, fileInfo => { throw new Exception("Shouldn't be called."); });
+
+            // Assert 2
+            Assert.Null(result2);
+        }
+
+        [Fact]
+        public void GetOrAdd_UpdatesCacheWithValue_IfFileWasAdded()
+        {
+            // Arrange
+            var path = @"Views\Home\_ViewStart.cshtml";
+            var fileProvider = new TestFileProvider();
+            var codeTreeCache = new CodeTreeCache(fileProvider);
+            var expected = new CodeTree();
+
+            // Act 1
+            var result1 = codeTreeCache.GetOrAdd(path, fileInfo => { throw new Exception("Shouldn't be called."); });
+
+            // Assert 1
+            Assert.Null(result1);
+
+            // Act 2
+            fileProvider.AddFile(path, "test content");
+            fileProvider.GetTrigger(path).IsExpired = true;
+            var result2 = codeTreeCache.GetOrAdd(path, fileInfo => expected);
+
+            // Assert 2
+            Assert.Same(expected, result2);
+        }
+    }
+}
